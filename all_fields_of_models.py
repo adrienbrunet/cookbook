fc400dfa488f865470566abad8ec6008d31a9a90
Adrien Brunet <abrunet@centrale-marseille.fr>
2014-09-11 17:32:19 +0200

    Update all_fields_of_models.py
    typo

--- all_fields_of_models.py
+++ all_fields_of_models.py
@@ -1,5 +1,5 @@
 from django.db.models import get_app, get_models
 
 app = get_app('my_application_name')
-  for model in get_models(appss):
+  for model in get_models(app):
     print model, model._meta.get_all_field_names()
