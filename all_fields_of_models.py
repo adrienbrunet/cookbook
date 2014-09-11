1b6fc6db88227e480b440918ca6f75d7601ba668
Adrien Brunet <abrunet@centrale-marseille.fr>
2014-09-11 17:37:49 +0200

    Update all_fields_of_models.py
    adding .__class__ for readability

--- all_fields_of_models.py
+++ all_fields_of_models.py
@@ -2,4 +2,4 @@ from django.db.models import get_app, get_models
 
 app = get_app('my_application_name')
   for model in get_models(app):
-    print model, model._meta.get_all_field_names()
+    print model.__class__, model._meta.get_all_field_names()
